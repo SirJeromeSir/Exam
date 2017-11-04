ed48f4c7153292df47b78670badf784c19d5adeb
SirJeromeSir <33443680+SirJeromeSir@users.noreply.github.com>
2017-11-04 08:41:34 +0800

    OUT

---
+++ plugins.js
@@ -0,0 +1,7 @@
+$("#selectbox").on("change", function() {
+	var sOptionVal = $(this).val();
+	if (/modal/i.test(sOptionVal)) {
+	 var $selectedOption = $(sOptionVal);
+	 $selectedOption.modal('show');
+	}
+});
